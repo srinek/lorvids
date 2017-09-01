f5f3aa5202f2da258ebb25bdad510ee98adc4f53
Nandakishore Nekkanti <srinek@gmail.com>
2017-09-01 16:36:48 -0400

    search results

---
+++ angular-ui/src/app/model/business.model.ts
@@ -0,0 +1,11 @@
+
+export class Business{
+
+    id : number;
+    name : string;
+    address : string;
+    imageurl : string;
+    rating : number[];
+
+
+}
---
+++ angular-ui/src/app/service/appointment.service.ts
@@ -0,0 +1,47 @@
+import { Injectable }    from '@angular/core';
+
+import {Business} from '../model/business.model';
+import {Logger} from './logger.service';
+
+@Injectable()
+export class AppointmentService {
+
+    constructor(private logger: Logger){
+
+    }
+
+    private rating : number[] = [0 , 0 , 0 ,0 ];
+
+    private trendingBusiness : Business[] = [
+        {id: 1, name : "Smile Dental", address : "110 Durham Road, Piscataway", imageurl : "", rating : this.rating},
+        {id: 2, name : "Bubbles Pediatrician", address : "1145 fifth avenue, Edison", imageurl : "", rating : this.rating},
+        {id: 3, name : "Trendy Looks (Salon)", address : "1145 fifth avenue, Edison", imageurl : "", rating : this.rating},
+        {id: 4, name : "A1 Cleaners", address : "1145 fifth avenue, Edison", imageurl : "", rating : this.rating}
+    ];
+
+    private recentlyVisitedBusiness : Business[] = [
+        {id: 1, name : "Smile Dental", address : "110 Durham Road, Piscataway", imageurl : "", rating : this.rating},
+        {id: 2, name : "Bubbles Pediatrician", address : "1145 fifth avenue, Edison", imageurl : "", rating : this.rating},
+        {id: 3, name : "Trendy Looks (Salon)", address : "1145 fifth avenue, Edison", imageurl : "", rating : this.rating},
+        {id: 4, name : "A1 Cleaners", address : "1145 fifth avenue, Edison", imageurl : "", rating : this.rating}
+    ];
+
+    private searchResults : Business[] = [
+        {id: 1, name : "Smile Dental", address : "110 Durham Road, Piscataway", imageurl : "../../assets/trendy_looks.jpg", rating : this.rating},
+        {id: 2, name : "Bubbles Pediatrician", address : "1145 fifth avenue, Edison", imageurl : "../../assets/home_cleaning.jpg", rating : this.rating},
+        {id: 3, name : "Trendy Looks (Salon)", address : "1145 fifth avenue, Edison", imageurl : "../../assets/trendy_looks.jpg", rating : this.rating},
+        {id: 4, name : "A1 Cleaners", address : "1145 fifth avenue, Edison", imageurl : "../../assets/home_cleaning.jpg", rating : this.rating}
+    ];
+
+    public getTredingBusiness() : Business[] {
+        return this.trendingBusiness;
+    }
+
+    public getRecentlyVisitedBusiness() : Business[] {
+        return this.recentlyVisitedBusiness;
+    }
+
+    public getSearchResults(searchTerm : string) : Business[] {
+        return this.searchResults;
+    }
+}
