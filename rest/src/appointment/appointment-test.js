4f16b4e2275b0852ef55a4af4cb159d72922dc52
Nandakishore Nekkanti <srinek@gmail.com>
2017-10-23 14:38:31 -0400

    corrected test data

--- rest/src/appointment/appointment-test.js
+++ rest/src/appointment/appointment-test.js
@@ -1,26 +1,21 @@
 var saveAppointment = require('./saveAppointment');
 saveAppointment.saveAppointment(
-    {
-        "queryStringParameters": {
-          "saveuser": "true"
-        },
-        "body": {
-          "appt": {
-            "StaffId": "b-test-01-s-02",
-            "UserEmail": "nanda@gmail.com",
-            "apptId": "b-test-01-s-01-a-1400",
-            "busId": "b-test-01",
-            "location": "business address",
-            "notes": "empty",
-            "service": "dental cleaning"
-          },
-          "user": {
-            "UserEmail": "nanda1@gmail.com",
-            "name": "nanda1 nekkanti",
-            "phone": "9784441121"
-          }
-        }
-    }, 
+  {
+    "appt": {
+    "StaffId": "b-test-02-s-02",
+    "UserEmail": "ritesh@gmail.com",
+    "apptId": "b-test-02-s-02-a-1400",
+    "busId": "b-test-02",
+    "location": "business address",
+    "notes": "empty",
+    "service": "dental cleaning"
+    },
+    "user": {
+    "UserEmail": "retesh@gmail.com",
+    "name": "ritesh nekkanti",
+    "phone": "9784441121"
+    }
+  }, 
     null, 
     (error, response) => {
         console.log('-----------------------------SAVE RESPONSE START-----------------------------');
