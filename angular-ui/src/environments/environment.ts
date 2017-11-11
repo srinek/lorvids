d35e2dc5e9c38b2a2812bac71f36dca08384540b
Nandakishore Nekkanti <srinek@gmail.com>
2017-11-10 20:16:53 -0500

    externalizing properties

--- angular-ui/src/environments/environment.ts
+++ angular-ui/src/environments/environment.ts
@@ -4,8 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  appurl : "https://htb4u6t274.execute-api.us-east-1.amazonaws.com/dev/",
-  searchurl : "https://search-epokly-dev-v1-akgmth4zxz676zcoosjtnrkdfi.us-east-1.es.amazonaws.com/business_docs/_search"
-
+    production: false,
+    appurl : "https://htb4u6t274.execute-api.us-east-1.amazonaws.com/dev/",
+    searchurl : "https://search-epokly-dev-v1-akgmth4zxz676zcoosjtnrkdfi.us-east-1.es.amazonaws.com/business_docs/_search"
 };
